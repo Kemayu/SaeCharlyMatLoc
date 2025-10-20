c83409f264adfcdcaf3e86278669e7ee5f0d73d7
Paul5400 <paul.andrieu54@gmail.com>
2025-10-20 15:44:44 +0200

    feat (routes)

--- back.charlyMatLoc/app/src/api/routes.php
+++ back.charlyMatLoc/app/src/api/routes.php
@@ -3,16 +3,15 @@
 declare(strict_types=1);
 
 use charlyMatL\application\actions\GetCatalogAction;
-use charlyMatL\application\actions\GetToolByIdAction;
+use charlyMatL\application\actions\GetToolDetailsAction;
 use Slim\App;
 
 return function(App $app): App {
-
-    // $app->get('/', HomeAction::class);
-
-
+    // Route pour récupérer le catalogue des outils
     $app->get('/tools', GetCatalogAction::class);
-    $app->get('/tools/{id}', GetToolByIdAction::class);
+
+    // Route pour récupérer les détails d'un outil spécifique
+    $app->get('/tools/{id}', GetToolDetailsAction::class);
 
     return $app;
 };
