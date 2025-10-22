6688b577cd0c89a534bd34679835d2e398df5331
Paul5400 <paul.andrieu54@gmail.com>
2025-10-22 14:35:39 +0200

    fix (exceptions)

--- back.charlyMatLoc/app/src/api/actions/GetCartDetailsAction.php
+++ back.charlyMatLoc/app/src/api/actions/GetCartDetailsAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace charlymatloc\api\actions;
 
+use Slim\Exception\HttpInternalServerErrorException;
 use charlymatloc\core\ports\spi\ServiceCartInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -19,25 +20,29 @@ final class GetCartDetailsAction extends AbstractAction
 
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        $userId = $request->getQueryParams()['user_id'] ?? 'guest';
+        try {
+            $userId = $request->getQueryParams()['user_id'] ?? 'guest';
 
-        $cartDTO = $this->cartService->getCurrentCart($userId);
+            $cartDTO = $this->cartService->getCurrentCart($userId);
 
-        $data = [
-            'success' => true,
-            'cart' => [
-                'items_count' => count($cartDTO->items),
-                'total' => $cartDTO->total,
-                'items' => array_map(fn($item) => [
-                    'tool' => $item->tool,
-                    'quantity' => $item->quantity
-                ], $cartDTO->items)
-            ]
-        ];
+            $data = [
+                'success' => true,
+                'cart' => [
+                    'items_count' => count($cartDTO->items),
+                    'total' => $cartDTO->total,
+                    'items' => array_map(fn($item) => [
+                        'tool' => $item->tool,
+                        'quantity' => $item->quantity
+                    ], $cartDTO->items)
+                ]
+            ];
 
-        $response->getBody()->write(json_encode($data));
-        return $response
-            ->withHeader('Content-Type', 'application/json')
-            ->withStatus(200);
+            $response->getBody()->write(json_encode($data));
+            return $response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(200);
+        } catch (\Exception $e) {
+            throw new HttpInternalServerErrorException($request, $e->getMessage());
+        }
     }
 }
