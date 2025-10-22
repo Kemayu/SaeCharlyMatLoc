c9d014cd3d65f2cab1ea19f9a382ca1902db9b5f
MiracOK <mirac.demirci2@etu.univ-lorraine.fr>
2025-10-22 09:53:18 +0200

    fix (api) : route api pour en local

--- back.charlyMatLoc/app/config/api.php
+++ back.charlyMatLoc/app/config/api.php
@@ -15,7 +15,7 @@ return [
     Cors::class => function (ContainerInterface $c) {
 
         return new Cors(
-            allowedOrigins: ['http://docketu.iutnc.univ-lorraine.fr:48210'],  // ['http://localhost:48210']
+            allowedOrigins: ['http://docketu.iutnc.univ-lorraine.fr:48210', 'http://localhost:48210'],  // ['http://localhost:48210']
             allowedMethods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
             allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
             maxAge: 3600,  //1h
